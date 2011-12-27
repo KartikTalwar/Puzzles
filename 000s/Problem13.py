0e895dcee3756ef9363645ed47c15c9fb7c8b0a9
Kartik <talwar.kartik@gmail.com>
2011-12-27 01:00:40 -0500

    Problem 13

--- 000s/Problem13.py
+++ 000s/Problem13.py
@@ -1,3 +1,11 @@
+"""
+/*
+
+    Work out the first ten digits of the sum of the following one-hundred 50-digit numbers.
+
+*/
+"""
+
 import urllib as get
 
 data = get.urlopen("https://raw.github.com/gist/1522808/e3b3e9f6c95633296dd9bfeebfd435469b6eead4/ProjectEuler13.txt")
