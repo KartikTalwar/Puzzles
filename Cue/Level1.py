e410ece063cd36db340af78e6317a2e9a7d33c38
Kartik Talwar <hi@kartikt.com>
2013-03-25 06:34:18 +0000

    Geplin programming challenge documentation

--- Cue/Level1.py
+++ Cue/Level1.py
@@ -1,5 +1,16 @@
+"""
+Level 1
 
-# Find the longest substring that is the same in reverse
+----------------------------------------
+
+Embedded in this block of text is the password for level 2.
+The password is the longest substring that is the same in reverse.
+
+As an example, if the input was "I like racecars that go fast"
+the password would be "racecar".
+"""
+
+
 def longestPalindrome(str):
     best = 0
     pos  = 0
